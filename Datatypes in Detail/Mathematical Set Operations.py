f3ef4b108e71eda771ee6ed60b33220d2f65dfa3
priyanshu-1368 <im6575330@gmail.com>
2026-09-18 18:03:20 +0530

    datatypes in detail

---
+++ Datatypes in Detail/Mathematical Set Operations.py
@@ -0,0 +1,11 @@
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+union_set = set_a.union(set_b)
+print("Union:", union_set)
+
+intersection_set =  set_a.intersection(set_b)
+print("Intersection:", intersection_set)
+
+difference_set = set_a.difference(set_b)
+print("Difference (A - B):", difference_set)
